8b15ed4d34267b81c28af47049e6c60ea7574dfb
mohadelrezk <moh.adelrezk@gmail.com>
2018-10-03 18:00:09 +0100

    initial cleaning - code restructure

--- pipeline_components/a_marineDataAPI/marineAPI.py
+++ pipeline_components/a_marineDataAPI/marineAPI.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.insert(0, '../shared')
+
 import csv
 import json
 #import requests
@@ -10,51 +13,68 @@ from util.mLogger import logee
 #vars
 from util.config import log_dir, log_name, log_file
 
+# objects
+from util.urlEncode import EncodeDecode
+# from marineDataAPI.marineAPI import MarineAPI
+from util.time import TimeOperations
+# vars
+from util.config import ds_names
 
-class MarineAPI:
 
 
+class MarineDataPortalApi(object):
 
-    # constructor
     def __init__(self):
 
+
+        self.encode = EncodeDecode()
+        self.time = TimeOperations()
         # initiate logger object
-        global log
-        log = logee(log_dir, log_file, log_name)
-
-        global file_sys
-        file_sys = FileOperations()
-
+        self.log = logee(log_dir, log_file, log_name)
+        self.file_sys = FileOperations()
         # times place holders
-        global time_from
-        time_from = ""
-        global time_to
-        time_to = ""
+        self.time_from = ""
+        self.time_to = ""
 
-        # datasets APIs
-        """
-        global url_csv_IMIEATLWAVE
-        url_csv_IMIEATLWAVE = "https://erddap.marine.ie/erddap/griddap/IMI_EATL_WAVE.csv?significant_wave_height[(" + time_from + "):1:(" + time_to + ")][(36.5125):1:(59.987500000000004)][(-19.9875):1:(-0.01249999999999929)],swell_wave_height[(" + time_from + "):1:(" + time_to + ")][(36.5125):1:(59.987500000000004)][(-19.9875):1:(-0.01249999999999929)],mean_wave_direction[(" + time_from + "):1:(" + time_to + ")][(36.5125):1:(59.987500000000004)][(-19.9875):1:(-0.01249999999999929)],mean_wave_period[(" + time_from + "):1:(" + time_to + ")][(36.5125):1:(59.987500000000004)][(-19.9875):1:(-0.01249999999999929)]"
-
-        global url_json_IMIEATLWAVE
-        url_json_IMIEATLWAVE = "https://erddap.marine.ie/erddap/griddap/IMI_EATL_WAVE.json?significant_wave_height[(2017-06-12T21:00:00Z):1:(2017-06-19T21:00:00Z)][(36.5125):1:(59.987500000000004)][(-19.9875):1:(-0.01249999999999929)],swell_wave_height[(2017-06-12T21:00:00Z):1:(2017-06-19T21:00:00Z)][(36.5125):1:(59.987500000000004)][(-19.9875):1:(-0.01249999999999929)],mean_wave_direction[(2017-06-12T21:00:00Z):1:(2017-06-19T21:00:00Z)][(36.5125):1:(59.987500000000004)][(-19.9875):1:(-0.01249999999999929)],mean_wave_period[(2017-06-12T21:00:00Z):1:(2017-06-19T21:00:00Z)][(36.5125):1:(59.987500000000004)][(-19.9875):1:(-0.01249999999999929)]"
-        """
-        global url_csv_IWBNetwork
-        url_csv_IWBNetwork = "https://erddap.marine.ie/erddap/tabledap/IWBNetwork.csv0?station_id%2Clongitude%2Clatitude%2Ctime%2CAtmosphericPressure%2CWindDirection%2CWindSpeed%2CGust%2CWaveHeight%2CWavePeriod%2CMeanWaveDirection%2CHmax%2CAirTemperature%2CDewPoint%2CSeaTemperature%2Csalinity%2CRelativeHumidity%2CQC_Flag&time%3E=" + time_from
-
-        global url_csv_IWaveBNetwork_spectral
-        url_csv_IWaveBNetwork_spectral = "https://erddap.marine.ie/erddap/tabledap/IWaveBNetwork_spectral.csv0?buoy_id%2Ctime%2Clatitude%2Clongitude%2Cstation_id%2CPeakDirection%2CPeakSpread%2CSignificantWaveHeight%2CEnergyPeriod%2CMeanWavePeriod_Tm01%2CMeanWavePeriod_Tm02%2CPeakPeriod%2Cqcflag&time%3E=" + time_from
-
-        global url_csv_IWaveBNetwork_zerocrossing
-        url_csv_IWaveBNetwork_zerocrossing = "https://erddap.marine.ie/erddap/tabledap/IWaveBNetwork_zerocrossing.csv0?buoy_id%2Ctime%2Clatitude%2Clongitude%2Cstation_id%2CHmax%2CHmaxPeriod%2CHavg%2CTavg%2Cqcflag&time%3E=" + time_from
-
-        global url_csv_IrishNationalTideGaugeNetwork
-        url_csv_IrishNationalTideGaugeNetwork = "https://erddap.marine.ie/erddap/tabledap/IrishNationalTideGaugeNetwork.csv0?longitude%2Clatitude%2Caltitude%2Ctime%2Cstation_id%2CWater_Level%2CWater_Level_LAT%2CWater_Level_OD_Malin%2CQC_Flag&time%3E=" + time_from
-
+        # datasets APIs/URLs
+        self.url_csv_IWBNetwork = "https://erddap.marine.ie/erddap/tabledap/IWBNetwork.csv0?station_id%2Clongitude%2Clatitude%2Ctime%2CAtmosphericPressure%2CWindDirection%2CWindSpeed%2CGust%2CWaveHeight%2CWavePeriod%2CMeanWaveDirection%2CHmax%2CAirTemperature%2CDewPoint%2CSeaTemperature%2Csalinity%2CRelativeHumidity%2CQC_Flag&time%3E=" + time_from
+        self.url_csv_IWaveBNetwork_spectral = "https://erddap.marine.ie/erddap/tabledap/IWaveBNetwork_spectral.csv0?buoy_id%2Ctime%2Clatitude%2Clongitude%2Cstation_id%2CPeakDirection%2CPeakSpread%2CSignificantWaveHeight%2CEnergyPeriod%2CMeanWavePeriod_Tm01%2CMeanWavePeriod_Tm02%2CPeakPeriod%2Cqcflag&time%3E=" + time_from
+        self.url_csv_IWaveBNetwork_zerocrossing = "https://erddap.marine.ie/erddap/tabledap/IWaveBNetwork_zerocrossing.csv0?buoy_id%2Ctime%2Clatitude%2Clongitude%2Cstation_id%2CHmax%2CHmaxPeriod%2CHavg%2CTavg%2Cqcflag&time%3E=" + time_from
+        self.url_csv_IrishNationalTideGaugeNetwork = "https://erddap.marine.ie/erddap/tabledap/IrishNationalTideGaugeNetwork.csv0?longitude%2Clatitude%2Caltitude%2Ctime%2Cstation_id%2CWater_Level%2CWater_Level_LAT%2CWater_Level_OD_Malin%2CQC_Flag&time%3E=" + time_from
         # deprecated dataset
-        url_json_IWaveBNetwork30Min = "https://erddap.marine.ie/erddap/tabledap/IWaveBNetwork30Min.json?longitude%2Clatitude%2Ctime%2Cstation_id%2CPeakPeriod%2CPeakDirection%2CUpcrossPeriod%2CSignificantWaveHeight%2CSeaTemperature%2CHmax%2CTHmax%2CMeanCurDirTo%2CMeanCurSpeed%2CSignificantWaveHeight_qc%2CPeakPeriod_qc%2CPeakDirection_qc%2CUpcrossPeriod_qc%2CSeaTemperature_qc%2CHmax_qc%2CTHmax_qc%2CMeanCurDirTo_qc%2CMeanCurSpeed_qc&time%3E="  # 2017-06-07T00%3A00%3A00Z"
+        self.url_json_IWaveBNetwork30Min = "https://erddap.marine.ie/erddap/tabledap/IWaveBNetwork30Min.json?longitude%2Clatitude%2Ctime%2Cstation_id%2CPeakPeriod%2CPeakDirection%2CUpcrossPeriod%2CSignificantWaveHeight%2CSeaTemperature%2CHmax%2CTHmax%2CMeanCurDirTo%2CMeanCurSpeed%2CSignificantWaveHeight_qc%2CPeakPeriod_qc%2CPeakDirection_qc%2CUpcrossPeriod_qc%2CSeaTemperature_qc%2CHmax_qc%2CTHmax_qc%2CMeanCurDirTo_qc%2CMeanCurSpeed_qc&time%3E="  # 2017-06-07T00%3A00%3A00Z"
+        self.url_csv_IWaveBNetwork30Min = "https://erddap.marine.ie/erddap/tabledap/IWaveBNetwork30Min.csv?longitude%2Clatitude%2Ctime%2Cstation_id%2CPeakPeriod%2CPeakDirection%2CUpcrossPeriod%2CSignificantWaveHeight%2CSeaTemperature%2CHmax%2CTHmax%2CMeanCurDirTo%2CMeanCurSpeed%2CSignificantWaveHeight_qc%2CPeakPeriod_qc%2CPeakDirection_qc%2CUpcrossPeriod_qc%2CSeaTemperature_qc%2CHmax_qc%2CTHmax_qc%2CMeanCurDirTo_qc%2CMeanCurSpeed_qc&time%3E="  # 2017-06-07T00%3A00%3A00Z"
 
-        url_csv_IWaveBNetwork30Min = "https://erddap.marine.ie/erddap/tabledap/IWaveBNetwork30Min.csv?longitude%2Clatitude%2Ctime%2Cstation_id%2CPeakPeriod%2CPeakDirection%2CUpcrossPeriod%2CSignificantWaveHeight%2CSeaTemperature%2CHmax%2CTHmax%2CMeanCurDirTo%2CMeanCurSpeed%2CSignificantWaveHeight_qc%2CPeakPeriod_qc%2CPeakDirection_qc%2CUpcrossPeriod_qc%2CSeaTemperature_qc%2CHmax_qc%2CTHmax_qc%2CMeanCurDirTo_qc%2CMeanCurSpeed_qc&time%3E="  # 2017-06-07T00%3A00%3A00Z"
+    def start_harvesting(self):
+
+        # prepare time
+        timeStartFrom_encoded = self.time.getTimeFromEncoded()
+        timeEndAt_encoded = self.time.getTimeToEncoded()
+
+        timeStartFrom = self.time.getTimeFrom()
+        timeEndAt = self.getTimeTo()
+
+        # prepare datasets names
+        ds_names_list = ds_names.values()
+
+        # In Python 3
+        # list(d.values())
+
+        # harvet data
+        for ds in ds_names_list:
+            apiURL = marineAPI.getAPIurl(ds, timeStartFrom_encoded, timeEndAt_encoded)
+            print("API URL arguments :", apiURL)
+            csvData = marineAPI.getCSV(apiURL)
+
+            # for row in csvData:
+            #    print row
+
+            csvfileName = ds + "__SRARTED__" + timeStartFrom + "__TO__" + timeEndAt
+            message = marineAPI.putCSVToFile(csvData, csvfileName)
+
+            if message:
+                print
+                message
 
     #not finished yet as not in scope
     def getJSON(self,api):
@@ -128,3 +148,7 @@ class MarineAPI:
 
             print url_csv_IWBNetwork+time_from
             return url_csv_IWBNetwork+time_from
+
+if __name__ == "__main__":
+    marineAPI = MarineDataPortalApi()
+    marineAPI.start_harvesting()
--- shared/util/time.py
+++ shared/util/time.py
@@ -1,5 +1,3 @@
-
-
 import datetime
 import datetime
 import schedule
@@ -8,10 +6,7 @@ import urllib
 from datetime import timedelta
 
 
-class TimeOperations:
-
-
-
+class TimeOperations(object):
     def encode(self, URL):
         URI_encoded = urllib.quote_plus(URL)
         #   print URI_encoded
@@ -22,26 +17,24 @@ class TimeOperations:
         #   print URI_encoded
         return URI_decoded
 
-    #get yesterday time
+    # get yesterday time
     def getTimeFromEncoded(self):
-
-
         time_from = "2010-01-01T00%3A00%3A00Z"
 
         # print encode(time_from)
 
         # print decode(time_from)
 
-        #print time_from
-        #print datetime.datetime.now().replace(microsecond=0).isoformat()
-        #print self.encode(datetime.datetime.now().replace(microsecond=0).isoformat())
+        # print time_from
+        # print datetime.datetime.now().replace(microsecond=0).isoformat()
+        # print self.encode(datetime.datetime.now().replace(microsecond=0).isoformat())
 
         yesterday = datetime.datetime.now() - timedelta(days=1)
-        #yesterday.strftime('%m%d%y')
+        # yesterday.strftime('%m%d%y')
 
         print yesterday
 
-        #print self.encode(yesterday.replace(microsecond=0).isoformat())
+        # print self.encode(yesterday.replace(microsecond=0).isoformat())
         # @TODO uncomment after initial download
         """time_from = self.encode(yesterday.replace(microsecond=0).isoformat())"""
 
@@ -51,49 +44,44 @@ class TimeOperations:
 
     # get today time
     def getTimeToEncoded(self):
-
         time_to = "time%3E=2017-06-07T00%3A00%3A00Z"
 
         # print encode(time_from)
 
         # print decode(time_from)
 
-        #print time_to
-        #print datetime.datetime.now().replace(microsecond=0).isoformat()
-        #print self.encode(datetime.datetime.now().replace(microsecond=0).isoformat())
+        # print time_to
+        # print datetime.datetime.now().replace(microsecond=0).isoformat()
+        # print self.encode(datetime.datetime.now().replace(microsecond=0).isoformat())
 
         today = datetime.datetime.now()
-        #today.strftime('%m%d%y')
+        # today.strftime('%m%d%y')
 
         print today
 
-        #print self.encode(today.replace(microsecond=0).isoformat())
+        # print self.encode(today.replace(microsecond=0).isoformat())
 
         time_to = self.encode(today.replace(microsecond=0).isoformat())
 
         print time_to
 
-
         return time_to
 
     def getTimeFrom(self):
-
-
         time_from = "2010-01-01T00%3A00%3A00Z"
 
         # print encode(time_from)
 
-        #print decode(time_from)
+        # print decode(time_from)
 
-        #print time_from
-
+        # print time_from
 
         yesterday = datetime.datetime.now() - timedelta(days=1)
-        #yesterday.strftime('%m%d%y')
+        # yesterday.strftime('%m%d%y')
 
         print yesterday
 
-        #@TODO uncomment after initial download
+        # @TODO uncomment after initial download
         """time_from = yesterday.replace(microsecond=0).isoformat()"""
 
         print time_from
@@ -101,7 +89,6 @@ class TimeOperations:
         return time_from
 
     def getTimeTo(self):
-
         time_to = "time%3E=2017-06-07T00%3A00%3A00Z"
 
         # print encode(time_from)
@@ -123,14 +110,12 @@ class TimeOperations:
 
         print self.decode(time_to)
 
-
-
         return self.decode(time_to)
 
 
-test = TimeOperations()
-
-#test.getTimeToEncoded()
-#test.getTimeTo()
-test.getTimeFrom()
-test.getTimeFromEncoded()
+if __name__ == "__main__":
+    test = TimeOperations()
+    # test.getTimeToEncoded()
+    # test.getTimeTo()
+    test.getTimeFrom()
+    test.getTimeFromEncoded()
